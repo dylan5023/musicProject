024ae2cf4972ab6fb96aa886b703b27f2e85ca5c
Guilherme Forssetto <guiforssetto07@gmail.com>
2023-05-05 15:25:32 -0700

    section updated to react

---
+++ src/components/Page1.js
@@ -0,0 +1,5 @@
+export function Page1(){
+    return(
+        <h1>Home/Charts Page</h1>
+    );
+};
---
+++ src/components/Page2.js
@@ -0,0 +1,5 @@
+export function Page2(){
+    return(
+        <h2>Create Playlist Page</h2>
+    );
+};

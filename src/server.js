14b91516bbebb5bf75e827336b043d5c3dd7d01f
Dylan5023 <kangjun5023@gmail.com>
2023-05-05 15:26:25 -0700

    create server.js

---
+++ src/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const usersRouter = require("./routes/users.router");
+const PORT = process.env.PORT;
+const app = express();
+
+const { default: mongoose } = require("mongoose");
+mongoose
+  .connect(`process.env.MONGODB_URI`)
+  .then(() => console.log("mongodb connected"))
+  .catch((err) => console.error(err));
+
+app.use((req, res, next) => {
+  const start = Date.now();
+  console.log(`start: ${req.method} ${req.url}`);
+  next();
+  const diffTime = Date.now() - start;
+  console.log(`end: ${req.method} ${req.baseUrl}${req.url} ${diffTime}ms`);
+});
+
+app.use("/users", usersRouter);
+app.use("/tracks", tracksRouter);
+
+app.listen(PORT, () => {
+  console.log(`Running on port ${PORT}`);
+});

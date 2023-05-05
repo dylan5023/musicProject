d0c81a9a8b40952f87e73c993f74947a35c4486a
Dylan5023 <kangjun5023@gmail.com>
2023-05-04 18:41:46 -0700

    Clear input error and add favicon

--- js/main.js
+++ js/main.js
@@ -50,27 +50,32 @@ loginForm.submit((e) => {
 });
 //Creating a new user
 createAccount.submit((e) => {
-    e.preventDefault();
-    const name = $(".input").eq(0).val();
-    const password = $(".input").eq(1).val();
-    const confirmPassword = $(".input").eq(2).val();  
-    userId += userList.size;
-	 
-	 //  Input error message
-	 if($("#password1").val() !== $("#password1").val()){
-		 $(".form-message").text("Passwords do not match");
-		 $(".form-message").addClass("form-message-error");
-		} else if(name === "" || password === "" || confirmPassword === ""){
-			setFormMessage("#createAccountForm", "error", "Please fill up the form");
-		} else {
-		  let newUser = new User(userId,name,password, confirmPassword);
-		  userList.set(newUser);
-		  $("#loginPage").hide();
-		  $("#mainPage").show();
-		  console.log(newUser);
-	  }
+  e.preventDefault();
+  const name = $(".input").eq(0).val();
+  const password = $(".input").eq(1).val();
+  const confirmPassword = $(".input").eq(2).val();
+  userId += userList.size;
 
-})
+  //  Input error message
+  if ($("#password1").val() !== $("#password1").val()) {
+    $(".form-message").text("Passwords do not match");
+    $(".form-message").addClass("form-message-error");
+  } else if (name === "" || password === "" || confirmPassword === "") {
+    setFormMessage("#createAccountForm", "error", "Please fill up the form");
+  } else {
+    let newUser = new User(userId, name, password, confirmPassword);
+    userList.set(newUser);
+    $("#loginPage").hide();
+    $("#mainPage").show();
+    console.log(newUser);
+  }
+});
+
+// Clear input error
+$(".input").on("input", () => {
+  $(".form-message").hide("form-message-error");
+});
+
 // Hide login and display create account form
 $(".noaccount").click((e) => {
   e.preventDefault();
@@ -92,7 +97,6 @@ function setFormMessage(formElement, type, message) {
   messageElement.removeClass("form-message-success", "form-message-error");
   messageElement.addClass(`form-message-${type}`);
 }
-
 
 // get data from server
 // async function getData() {

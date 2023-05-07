8c09e27a1dae2f2f0beafabd1546a14c3ca071d9
Pedro Soares <pedrossgarcia88@gmail.com>
2023-05-07 15:17:36 -0700

    saving

--- js/main.js
+++ js/main.js
@@ -1,101 +1,101 @@
-import { node } from "prop-types";
-import "../scss/style.scss";
-import User from "./user.js";
-import Profile from "./componets/Profile";
-import CreateParty from "./componets/CreateParty";
-import Chart from "./componets/Chart";
+import { node } from 'prop-types';
+import '../scss/style.scss';
+import User from './user.js';
+import Profile from './componets/Profile';
+import CreateParty from './componets/CreateParty';
+import Chart from './componets/Chart';
 // Single Page Aplication
 
 const pages = [
-  { path: "#/page1", template: Chart },
-  { path: "#/page2", template: CreateParty },
-  { path: "#/page3", template: Profile },
+	{ path: '#/page1', template: Chart },
+	{ path: '#/page2', template: CreateParty },
+	{ path: '#/page3', template: Profile },
 ];
-const appEl = document.querySelector("#app");
+const appEl = document.querySelector('#app');
 
 const render = () => {
-  const page = pages.find((page) => page.path === location.hash);
-  appEl.innerHTML = page ? page.template : Chart;
+	const page = pages.find((page) => page.path === location.hash);
+	appEl.innerHTML = page ? page.template : Chart;
 };
 
-window.addEventListener("popstate", render);
+window.addEventListener('popstate', render);
 
 render();
 
 // Login Code
-const loginForm = $("#loginForm");
-const createAccount = $("#createAccountForm");
+const loginForm = $('#loginForm');
+const createAccount = $('#createAccountForm');
 let userList = new Map();
 let users = null;
 let userId = 1000;
 
 // Loading data from JSON file
-$.getJSON("http://localhost:8070/users", (data) => {
-  users = data;
-  console.log(users);
+$.getJSON('http://localhost:8070/users', (data) => {
+	users = data;
+	console.log(users);
 });
 // Checking user's credentials
 loginForm.submit((e) => {
-  e.preventDefault();
-  users.forEach((user) => {
-    if (
-      user.email == $(".input").eq(0).val() &&
-      user.password == $(".input").eq(1).val()
-    ) {
-      $("#loginPage").hide();
-      $("#mainPage").show();
-    }
-    setFormMessage("#loginForm", "error", "Invalid email/password combination");
-  });
+	e.preventDefault();
+	users.forEach((user) => {
+		if (
+			user.email == $('.input').eq(0).val() &&
+			user.password == $('.input').eq(1).val()
+		) {
+			$('#loginPage').hide();
+			$('#mainPage').show();
+		}
+		setFormMessage('#loginForm', 'error', 'Invalid email/password combination');
+	});
 });
 //Creating a new user
 createAccount.submit((e) => {
-  e.preventDefault();
-  const name = $(".input").eq(0).val();
-  const password = $(".input").eq(1).val();
-  const confirmPassword = $(".input").eq(2).val();
-  userId += userList.size;
+	e.preventDefault();
+	const name = $('.input').eq(0).val();
+	const password = $('.input').eq(1).val();
+	const confirmPassword = $('.input').eq(2).val();
+	userId += userList.size;
 
-  //  Input error message
-  if ($("#password1").val() !== $("#password1").val()) {
-    $(".form-message").text("Passwords do not match");
-    $(".form-message").addClass("form-message-error");
-  } else if (name === "" || password === "" || confirmPassword === "") {
-    setFormMessage("#createAccountForm", "error", "Please fill up the form");
-  } else {
-    let newUser = new User(userId, name, password, confirmPassword);
-    userList.set(newUser);
-    $("#loginPage").hide();
-    $("#mainPage").show();
-    console.log(newUser);
-  }
+	//  Input error message
+	if ($('#password1').val() !== $('#password1').val()) {
+		$('.form-message').text('Passwords do not match');
+		$('.form-message').addClass('form-message-error');
+	} else if (name === '' || password === '' || confirmPassword === '') {
+		setFormMessage('#createAccountForm', 'error', 'Please fill up the form');
+	} else {
+		let newUser = new User(userId, name, password, confirmPassword);
+		userList.set(newUser);
+		$('#loginPage').hide();
+		$('#mainPage').show();
+		console.log(newUser);
+	}
 });
 
 // Clear input error
-$(".input").on("input", () => {
-  $(".form-message").hide("form-message-error");
+$('.input').on('input', () => {
+	$('.form-message').hide('form-message-error');
 });
 
 // Hide login and display create account form
-$(".noaccount").click((e) => {
-  e.preventDefault();
-  loginForm.hide();
-  createAccount.show();
+$('.noaccount').click((e) => {
+	e.preventDefault();
+	loginForm.hide();
+	createAccount.show();
 });
 // Hide create account and display login form
-$(".account").click((e) => {
-  e.preventDefault();
-  createAccount.hide();
-  loginForm.show();
+$('.account').click((e) => {
+	e.preventDefault();
+	createAccount.hide();
+	loginForm.show();
 });
 
 // Setting the form message
 function setFormMessage(formElement, type, message) {
-  const messageElement = $(`${formElement} .form-message`);
+	const messageElement = $(`${formElement} .form-message`);
 
-  messageElement.text(message);
-  messageElement.removeClass("form-message-success", "form-message-error");
-  messageElement.addClass(`form-message-${type}`);
+	messageElement.text(message);
+	messageElement.removeClass('form-message-success', 'form-message-error');
+	messageElement.addClass(`form-message-${type}`);
 }
 
 // get data from server

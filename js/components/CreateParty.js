568ccbc284a4e52740381c84adecf67c295e366f
Guilherme Forssetto <guiforssetto07@gmail.com>
2023-05-11 03:31:26 -0700

    filter added

--- js/components/CreateParty.js
+++ js/components/CreateParty.js
@@ -1,5 +1,24 @@
-const CreateParty = /*html*/`
-   <article class="page1">
-      <h1>Create Party</h1>
-   </article>
-`
+const CreateParty = /* html */ `
+<form class="partyForm">
+<input type="text" id="partyName" name="partyName" placeholder="Party Name"/>
+<select>
+    <option>Private</option>
+    <option>Public</option>
+</select>
+<input type="submit" value="continue"/>
+</form>`;
+
+export default CreateParty;
+
+// export function FormParty(){
+
+// };
+
+
+export function Party() {
+    return (
+        <section>
+
+        </section>
+    );
+};
--- js/party.js
+++ js/party.js
@@ -2,14 +2,31 @@ import Tracks from "./tracks";
 const trackFile = "http://localhost:8070/tracks";
 let trackList = null;
 let queue = new Array();
+let names = [];
+let library = [];
 $.getJSON(trackFile, (data) => {
     trackList = data;
-    console.log(trackList)
+    trackList.forEach((track)=>{
+        names.push(track.track_name)
+    })
+    function getUnique(array){
+        var uniqueArray = [];
+        
+        for(var value of array){
+            if(uniqueArray.indexOf(value) === -1){
+                uniqueArray.push(value);
+            }
+        }
+        return uniqueArray;
+    }
+    library = getUnique(names);
+    console.log(library);
 });
+
 function showGuests() {
     $(".sideBar").slideToggle();
-    console.log("True")
-}
+    console.log("True");
+};
 
 function addMusic(e) {
     e.preventDefault();
@@ -19,14 +36,44 @@ function addMusic(e) {
         // console.log(val)
         let name = val.track_name.toLowerCase();
         if (search == name && search != "") {
-            let tempTrack = new Tracks(val.track_id,val.artists,val.track_name,val.album_name,val.track_genre,val.duration_ms,val.explicit)
+            let tempTrack = new Tracks(val.track_id,val.artists,val.track_name,val.album_name,val.track_genre,val.duration_ms,val.explicit);
             queue.push(tempTrack);
-            $(".queue").append(tempTrack.toQueue())
+            $(".queue").append(tempTrack.toQueue());
             return false;
         };
-        $(".musicInput").val("")
+        $(".musicInput").val("");
     }
 }
 
+
+function filterLibrary(){
+
+    // library.forEach((val)=>{
+
+    // });
+
+    let search = $(".musicInput").val().toLowerCase();
+    let counter = 0;
+    $(".filter").empty();
+    library.forEach((item)=>{
+        let text = item;
+        // console.log(text)
+        if(text.toLowerCase().includes(search) && search != "" && counter < 10){
+            let div = $("<div></div>");
+            div.text(item);
+            div.on("click",()=>{
+                $(".musicInput").val(text);
+                $(".filter").empty();
+            })
+            $(".filter").append(div);
+            counter++;
+        }
+        // else {
+        //     item.style.display = "none";
+        // }
+    });
+    // if(search == "")
+}
+$(".musicInput").on("keyup",filterLibrary);
 $(".hamburguerMenu").click(showGuests);
 $(".musicAdd").submit(addMusic);
